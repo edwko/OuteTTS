3e8b9c0f4a3837f8c3fc713cf280491f8e52ac29
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-04-08 13:18:09 -0600

    start to add async stuff (wip)

--- outetts/models/exl2_model.py
+++ outetts/models/exl2_model.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from tqdm import tqdm
+import asyncio
 
 from .config import GenerationConfig
 from .info import GenerationType
@@ -84,3 +85,95 @@ class EXL2Model:
         for i in self._generate_stream(input_ids, config):
             new_tokens.append(i)
         return new_tokens
+
+def run_loop(loop):
+    loop.run_forever()
+
+class Exl2AsyncLoop:
+    def __init__(self):
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=run_loop, args=[self.loop], daemon=True)
+        self.thread.start()
+
+class EXL2ModelAsync(EXL2Model):
+    def __init__(
+            self,
+            model_path: str,
+            max_seq_length: int = 4096,
+            additional_model_config: dict = {},
+            cache_impl: ExLlamaV2CacheBase = ExLlamaV2Cache,
+    ) -> None:
+
+        if not _EXL2_AVAILABLE:
+            raise ImportError(
+                "exllamav2 python module not found."
+                "To use the EXL2 model you must install exllamav2 manually."
+            )
+        
+        config = ExLlamaV2Config(model_path)
+        self.model = ExLlamaV2(config)
+        self.cache = cache_impl(self.model, max_seq_len=max_seq_length, lazy=True)
+        self.model.load_autosplit(self.cache, progress=True)
+        self.tokenizer = ExLlamaV2Tokenizer(config)
+        self.additional_dynamic_generator_config = additional_model_config.get("additional_dynamic_generator_config", {})
+        self.loop = EXL2AsyncLoop()
+        asyncio.run_coroutine_threadsafe(self._get_gen(), loop=self.loop.loop).result()
+
+    def _get_gen(self):
+        self.generator = ExLlamaV2DynamicGenerator(
+            model=self.model, cache=self.cache, tokenizer=self.tokenizer, **self.additional_dynamic_generator_config
+        )
+
+    async def _generate_async_job(self, chunk, idx, pbar):
+        job = ExLlamaV2DynamicJobAsync(
+            self.generator
+            input_ids=self.tokenizer.encode(chunk, add_bos=False, add_eos=False, encode_special_tokens=True),
+            max_new_tokens=config.max_length - input_size,
+            stop_conditions=[self.tokenizer.eos_token_id],
+            gen_settings=gen_settings,
+            decode_special_tokens=True,
+        ) 
+        async for result in job:
+            tokens = result.get("token_ids", "")
+            if tokens is None:
+                continue
+            yield tokens
+            pbar.update(1)
+    
+    def _generate_async(self, chunks, original, config: GenerationConfig):
+        gen_settings = ExLlamaV2Sampler.Settings(
+            token_repetition_penalty=config.sampler_config.repetition_penalty,
+            temperature=config.sampler_config.temperature,
+            token_repetition_range=config.sampler_config.repetition_range,
+            top_k=config.sampler_config.top_k,
+            top_p=config.sampler_config.top_p,
+            min_p=config.sampler_config.min_p,
+            mirostat_eta=config.sampler_config.mirostat_eta,
+            mirostat_tau=config.sampler_config.mirostat_tau,
+            mirostat=config.sampler_config.mirostat,
+            **config.additional_gen_config
+        )
+        tokens = self.tokenizer.encode(original, add_bos=False, add_eos=False, encode_special_tokens=True)
+        tokens_size = len(tokens)
+        input_size = len(tokens.flatten().tolist())
+        job = ExLlamaV2DynamicJob(
+            input_ids=tokens,
+            max_new_tokens=config.max_length - input_size,
+            stop_conditions=[self.tokenizer.eos_token_id],
+            gen_settings=gen_settings,
+            identifier=1,
+            decode_special_tokens=True,
+        )
+        self.generator.enqueue(job)
+        with tqdm(desc="Generating") as pbar:
+            while self.generator.num_remaining_jobs() > 0:
+                results = self.generator.iterate()
+                for result in results:
+                    token = result.get("token_ids", None)
+                    if token is None:
+                        continue
+                    pbar.update(1)
+                    pbar.set_postfix({"tokens": input_size + tokens_size, "max tokens": config.max_length})
+                    for t in token.flatten().tolist():
+                        yield t
+                        tokens_size += 1
